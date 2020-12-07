4a0b6ed2428e66523fc7a203804dfd7835a72402
Shikama190258 <190258gc@yse-c.net>
2020-12-07 11:43:31 +0900

    second commit

--- server.js
+++ server.js
@@ -5,6 +5,41 @@ const express = require('express');
 //サーバー作成
 const app = express();
 
+//JSON対応
+app.use(express.json());
+//URLエンコードされた
+app.use(express.urlencoded({ extended: true }));
+
+//クロスドメインの許可　XSS
+app.use((req, res, next) => {
+    console.log("middleware: all url: ${req.url}");
+
+
+    // CROS設定、全てのドメインに対して許可
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+
+    // 次の処理
+    next();
+
+
+});
+
+app.post("/", (req, res) => {
+
+    let message = req.body.message;
+
+    let result = {
+        // message: "Hello",
+        message: req.body.message, 
+        datetime: new Date(),
+    };
+    res.send(result);
+
+});
+
+
 //ルーティング
 // app.listen(3000);
 
